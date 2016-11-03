9899458d92c0d1c3dac60f4771258adb2fe92336
Pine Mizune <pinemz@gmail.com>
2016-11-03 09:56:32 +0900

    chore(repository): initial commit

---
+++ lib/JSON/Types/Flexible.pm
@@ -0,0 +1,39 @@
+package JSON::Types::Flexible;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+JSON::Types::Flexible - It's new $module
+
+=head1 SYNOPSIS
+
+    use JSON::Types::Flexible;
+
+=head1 DESCRIPTION
+
+JSON::Types::Flexible is ...
+
+=head1 LICENSE
+
+Copyright (C) Pine Mizune.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Pine Mizune E<lt>pinemz@gmail.comE<gt>
+
+=cut
+
