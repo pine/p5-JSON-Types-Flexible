3cb71c863ea3c422710a58ce57f8b54c869274e1
Pine Mizune <pinemz@gmail.com>
2016-11-03 16:31:03 +0900

    feat(Flexible): add initial code

--- cpanfile
+++ cpanfile
@@ -1,6 +1,21 @@
 requires 'perl', '5.008001';
 
+requires 'JSON::Types', '0.05';
+requires 'List::MoreUtils', '0.416';
+requires 'Sub::Install', '0.928';
+
 on 'test' => sub {
+    requires 'JSON::XS', '3.02';
+    requires 'Test::Exception', '0.43';
     requires 'Test::More', '0.98';
 };
 
+on 'develop' => sub {
+    requires 'Data::Printer', '0.38';
+    requires 'Minilla', '3.0.1';
+    requires 'Software::License::MIT', '0.103011';
+    requires 'Test::CPAN::Meta', '0.25';
+    requires 'Test::MinimumVersion::Fast', '0.04';
+    requires 'Test::PAUSE::Permissions', '0.05';
+    requires 'Test::Spellunker', '0.4.0';
+};
--- lib/JSON/Types/Flexible.pm
+++ lib/JSON/Types/Flexible.pm
@@ -1,11 +1,55 @@
 package JSON::Types::Flexible;
-use 5.008001;
 use strict;
 use warnings;
+use utf8;
 
 our $VERSION = "0.01";
 
+use JSON::Types ();
+use List::MoreUtils qw/uniq/;
+use Sub::Install;
 
+our %SUB_ALIAS = (
+    number  => 'number',
+    string  => 'string',
+    boolean => 'bool',
+    bool    => 'bool',
+);
+
+our @STRICT_SUBS = qw/
+    number
+    string
+    boolean
+/;
+
+our @LOOSE_SUBS = qw/
+    bool
+/;
+
+sub import {
+    my ($pkg, @args) = @_;
+    my $class = caller;
+
+    my @subs;
+    if (@args == 0) {
+        push @subs, @STRICT_SUBS;
+    }
+    elsif ($args[0] eq ':loose' || $args[0] eq ':all') {
+        push @subs, @STRICT_SUBS, @LOOSE_SUBS;
+    }
+    else {
+        push @subs, @args;
+    }
+
+    for (uniq @subs) {
+        Sub::Install::install_sub({
+          code => $SUB_ALIAS{$_},
+          from => 'JSON::Types',
+          into => $class,
+          as   => $_,
+        });
+    }
+}
 
 1;
 __END__
@@ -14,22 +58,61 @@ __END__
 
 =head1 NAME
 
-JSON::Types::Flexible - It's new $module
+JSON::Types::Flexible - Yet another L<JSON::Types> module
 
 =head1 SYNOPSIS
 
+    # Strict mode
     use JSON::Types::Flexible;
+
+    # Loose mode
+    use JSON::Types::Flexible ':loose';
 
 =head1 DESCRIPTION
 
-JSON::Types::Flexible is ...
+JSON::Types::Flexible is yet another L<JSON::Types> module.
+
+=head2 MODE
+=head3 Strict mode
+Export C<number>, C<string> and C<boolean> methods.
+
+=head3 Loose mode
+Export C<number>, C<string>, C<boolean> and C<bool> methods.
+
+=head2 METHODS
+=head3 number
+=head3 string
+=head3 bool
+
+See also L<JSON::Types>.
+
+=head3 boolean
+Alias for C<bool>.
 
 =head1 LICENSE
 
-Copyright (C) Pine Mizune.
+(The MIT license)
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+Copyright (c) 2016 Pine Mizune E<lt>pinemz@gmail.comE<gt>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 =head1 AUTHOR
 
